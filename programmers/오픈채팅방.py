5f0755f577e719d63afdf6b6b29d2e5ad1b8d864
hwstar-1204 <115150798+hwstar-1204@users.noreply.github.com>
2024-07-07 14:45:30 +0900

    1 solve

---
+++ programmers/오픈채팅방.py
@@ -0,0 +1,6 @@
+record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]	
+
+for r in record:
+    print(type(r.split()))
+
+assert  "op" is not "op"
