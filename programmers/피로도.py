a7c6af244c1ee8d2ad49c9b28bf0fdcc1f26323e
hwstar-1204 <hwstar1204@gmail.com>
2025-04-20 18:08:29 +0900

    1 re-solve

--- programmers/피로도.py
+++ programmers/피로도.py
@@ -1,26 +1,3 @@
-"""
-최소 필요 피로도 >= 소모 피로도 
-던전을 최대한 많이 탐험
-
-k : 유저 피로도 
-dungeons : 던전별 [최소 필요 피로도, 소요 피로도]
-
-return 탐험 최대 던전 수 
---
-
-dungeons 정렬 (최소 필요도가 큰 순서 , 소모 피로도가 작은 순서 )
-최소 필요도 - 소모피로도 가 큰 순서 라는거와 같은 이치 
-
-sudo code
-# 던전 입장
-for d in dungeons
-    # 던전 조건 확인 
-    if 최소 피로도 <= 현재 피로도 
-        # 피로도 계산 
-        현재 피로도 - 소모 피로도 
-        # 던전 입장수 + 1
-        count += 1
-"""
 # Loop version
 from itertools import permutations
 
@@ -28,7 +5,6 @@ def solution(k, dungeons):
     max_count = 0
     
     all_courses = permutations(dungeons, len(dungeons))
-    print(*all_courses)
 
     for course in all_courses:
         tmp_k = k # 60 20 
@@ -39,12 +15,10 @@ def solution(k, dungeons):
                 tmp_cnt += 1
 
         max_count = max(max_count, tmp_cnt)
-        if max_count == tmp_cnt:
-            print(c)
         
     return max_count
 
-# backtracking version
+# backtracking version 1
 def solution(k, dungeons):
     n = len(dungeons)
     max_count = 0
@@ -63,10 +37,38 @@ def solution(k, dungeons):
     backtrack(k, visited, 0)
     return max_count
 
+# backtracking version 2
+def solution(k, dungeons):
+    ans = 0
+    visited = [False] * len(dungeons)
+    
+    def backtrack(visited, now_k, count):
+        nonlocal ans
+        ans = max(ans, count)
 
-dungeons = [[80,20],[50,40],[30,10]]	
-# dungeons = [[80,20],[50,40],[30,10],[40,20]]
-k = 80
+        for i, (min_k, work_k) in enumerate(dungeons):
+            if now_k < min_k or visited[i]:
+                continue
 
-result = solution(k, dungeons) # 3
-assert result == 3
+            visited[i] = True
+            backtrack(visited, now_k-work_k, count+1)
+            visited[i] = False
+            
+    backtrack(visited, k, 0)
+    return ans
+
+# backtracking version 3
+def solution2(k, dungeons):
+    ans = 0
+    
+    def backtrack(remaining_dungeons, now_k, count):
+        nonlocal ans
+        ans = max(ans, count)
+
+        for i, (min_k, work_k) in enumerate(remaining_dungeons):
+            if now_k >= min_k:
+                next_remaining = remaining_dungeons[:i] + remaining_dungeons[i+1:] 
+                backtrack(next_remaining, now_k-work_k, count+1)
+            
+    backtrack(dungeons, k, 0)
+    return ans
