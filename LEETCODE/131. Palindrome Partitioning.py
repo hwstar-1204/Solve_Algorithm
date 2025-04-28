bbe8931ec4b57737d051014ab79b79ca8a74fa54
hwstar-1204 <hwstar1204@gmail.com>
2025-04-29 00:21:40 +0900

    Optimization

--- LEETCODE/131. Palindrome Partitioning.py
+++ LEETCODE/131. Palindrome Partitioning.py
@@ -6,9 +6,8 @@ class Solution:
         ans = []
 
         def backtrack(curr_s_list: List[str], start_idx: int):
-            if sum([len(s) for s in curr_s_list]) == len(s):
-                # if self.is_palindrome(curr_s_list):
-                ans.append(list(curr_s_list))
+            if start_idx == len(s):
+                ans.append(curr_s_list)                
                 return
 
             for end_idx in range(start_idx, len(s)):
@@ -21,16 +20,3 @@ class Solution:
 
         backtrack([], 0)
         return ans
-
-    def is_palindrome(self, s_list: List[str]) -> bool:
-        for s in s_list:
-            if s != s[::-1]:
-                return False
-        return True
-
-
-s = "a"
-solution = Solution()
-res = solution.partition(s)
-print(res)
-# assert res == [["a"]]
