20e7da5ed12cee91154209c2b01017eb95b152b9
hwstar-1204 <hwstar1204@gmail.com>
2025-05-24 23:54:20 +0900

    1 solve simulation

--- LEETCODE/0.qwer.py
+++ LEETCODE/0.qwer.py
@@ -1,3 +1,11 @@
-"""
-"""
-
+class Solution:
+    def complexNumberMultiply(self, num1: str, num2: str) -> str:
+        def parse(num):
+            real, imag = num[:-1].split("+")
+            return int(real), int(imag)
+        
+        a, b = parse(num1)
+        c, d = parse(num2)
+        real = a * c - b * d
+        imag = a * d + b * c
+        return f"{real}+{imag}i"
