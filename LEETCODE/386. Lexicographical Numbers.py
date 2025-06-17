d83bbaf2a96feb882af48a6854dddcac6559b6f4
hwstar-1204 <hwstar1204@gmail.com>
2025-06-17 22:09:44 +0900

    update

--- LEETCODE/386. Lexicographical Numbers.py
+++ LEETCODE/386. Lexicographical Numbers.py
@@ -1,10 +1,29 @@
 import heapq
 from typing import List
-
+# heap
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
         nums = list(map(str, [i for i in range(1,n+1)]))
         heapq.heapify(nums)
         return list(map(int, [heapq.heappop(nums) for _ in range(n)]))
 
-        
+# DFS
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        result = []
+        def dfs(curr):
+            if curr > n:
+                return
+            result.append(curr)
+            for i in range(10):
+                next_num = curr * 10 + i
+                if next_num > n:
+                    break
+                dfs(next_num)
+        for i in range(1, 10):
+            dfs(i)
+        return result
+
+n = 13
+s = Solution()
+print(s.lexicalOrder(n))
